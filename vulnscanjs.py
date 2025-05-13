012888cc13a9a3a4ef5d74f23c535cbdfab1713b
TechDevRon <103406560+TechDevRon@users.noreply.github.com>
2025-05-13 09:03:02 -0400

    added pipe

--- vulnscanjs.py
+++ vulnscanjs.py
@@ -5,6 +5,8 @@ from requests.exceptions import Timeout
 import argparse
 import re
 import queue
+import os
+import sys
 
 banner = f"""{Back.rgb('160', '32', '240')}
 ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░  ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓██████▓▒░ ░▒▓███████▓▒░        ░▒▓█▓▒░ ░▒▓███████▓▒░ 
@@ -23,18 +25,39 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 output_queue = queue.Queue()
 
 def initialize(urls_file, wordlist_file):
+
     wordlist_data = []
-    
-    with open(wordlist_file, 'r') as word_file:
-        for line in word_file:
-            wordlist_data.append(line.strip())
-    
-    with open(urls_file, 'r') as url_file:
-        for url in url_file:
-            urls_list.append(url.strip())
-    return wordlist_data
+
+    try:
+        if wordlist_file:
+            with open(wordlist_file, 'r') as word_file:
+                for line in word_file:
+                    wordlist_data.append(line.strip())
+
+        else:
+
+            for line in sys.stdin:
+                wordlist_data.append(line.strip().encode('utf-8'))
+
+    except Exception as e:
+
+        print(f'{Fore.red}{e}{Style.reset}')
+
+    try:
+        
+        with open(urls_file, 'r') as url_file:
+            for url in url_file:
+                urls_list.append(url.strip())
+        return wordlist_data
+
+    except Exception as e:
+
+        print(f'{Fore.red}{e}{Style.reset}')
+        os._exit(1)
+
 
 def perform_scan(url, wordlist_data, timeout, defaultmatching):
+
     output = [] 
     
     output.append(f"\n\n{Fore.blue}[*]Target:{Style.reset}{Fore.rgb('255', '165', '0')} {url}{Style.reset}")
@@ -48,17 +71,18 @@ def perform_scan(url, wordlist_data, timeout, defaultmatching):
     except requests.exceptions.RequestException as e:
         output.append(f"{Fore.red}Error: {e}{Style.reset}")
         output_queue.put('\n'.join(output)) 
-        return
+        os._exit(1)
 
     except KeyboardInterrupt as k:
         print("{Fore.red}Error: {e}")
         print(f"Keyboard Interruption Exiting.....{Style.reset}")
-        return
+        os._exit(1)
 
     if not wordlist_data:
         output.append(f"{Fore.red}Wordlist empty.{Style.reset}")
         output_queue.put('\n'.join(output))  
         return
+
     if defaultmatching  == True:
 
         for word in wordlist_data:
@@ -83,6 +107,7 @@ def perform_scan(url, wordlist_data, timeout, defaultmatching):
     output_queue.put('\n'.join(output))
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser("Js Scanner")
     parser.add_argument('-u', '--urls', type=str, help='File containing urls.')
     parser.add_argument('-w', '--wordlist', type=str, default='default/defaultWordlist', help='File of js functions to look for ex: eval. each word on different line.')
@@ -95,16 +120,19 @@ if __name__ == "__main__":
 
     print(f"{Fore.blue}[*]Starting...") 
     print(f"[*]Adding threads...")
+
     for url in urls_list:
         thread = threading.Thread(target=perform_scan, args=(url, wordlist, arguments.timeout, arguments.matching))
         threads.append(thread)
     
     print("[*]Starting threads...")
     print("[*]Connecting to sites, and scanning js, please be patience...")
+
     for thread in threads:
         thread.start()
     
     print(f"[*]Joining threads...{Style.reset}\n\n")
+
     for thread in threads:
         thread.join()  
 
